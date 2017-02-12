c47790d59a1ad911050785766b6e8336e0c518b5
Andrew <fuisseran@hotmail.com>
2017-02-12 14:16:50 +0000

    laoded model before delete for event firing to work properly

--- src/model/delete.js
+++ src/model/delete.js
@@ -2,6 +2,18 @@ var Promise = require('bluebird')
 var Modely = require('../index')
 var utils = require('./util')
 
+function checkModelIsLoaded(Model) {
+  return new Promise((resolve, reject) => {
+    if (Model._status !== 'loaded') {
+      Model.$read(Model[Model._primary_key])
+      .then(() => { return resolve() })
+      .catch(() => { reject(new Error('UnableToLoadModel')) })
+    } else {
+      return resolve()
+    }
+  })
+}
+
 /**
  * Deletes the model
  * ?@param {integer} model_id - id of the model to delete
@@ -9,7 +21,7 @@ var utils = require('./util')
  */
 function deleteModel(modelId) {
   // TODO: Need to get the transaction to appened to a passed transaction same as update and create
-  // TODO: Endure model has been loaded so that all events can fire propley when it is deleted.
+  // TODO: Ensure model has been loaded so that all events can fire propley when it is deleted.
   var Model = this  // Reference to the model
   var id = modelId
   return new Promise(function (resolve, reject) {
@@ -23,28 +35,31 @@ function deleteModel(modelId) {
     if (typeof Model[Model._primary_key] === 'undefined') {
       Model[Model._primary_key] = id
     }
-    Model.$processPending(Model, 'Delete')
-    return Modely.knex.transaction(function (trx) {
-      Model._trx = trx
-      return trx(Model._name)
-      .where(Model._columns[Model._primary_key].name, id)
-      .del()
-      .then(() => { return utils.pendingTransactions(Model, 'Delete') })
-      // .then(processRelationships)
-      .catch(reject)
+    checkModelIsLoaded(Model).then(() => {
+      Model.$processPending(Model, 'Delete')
+      return Modely.knex.transaction(function (trx) {
+        Model._trx = trx
+        return trx(Model._name)
+        .where(Model._columns[Model._primary_key].name, id)
+        .del()
+        .then(() => { return utils.pendingTransactions(Model, 'Delete') })
+        // .then(processRelationships)
+        .catch(reject)
+      })
+      .then(() => {
+        Model._action = null
+        return resolve()
+      })
+      .catch(error => {
+        Modely.log.error(error)
+        return reject(error)
+      })
+      .finally(() => {
+        Model._action = null
+        return resolve()
+      })
     })
-    .then(() => {
-      Model._action = null
-      return resolve()
-    })
-    .catch(error => {
-      Modely.log.error(error)
-      return reject(error)
-    })
-    .finally(() => {
-      Model._action = null
-      return resolve()
-    })
+    .catch(error => reject(error))
   })
 }
 
